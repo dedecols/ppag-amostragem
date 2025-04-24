78bc32e678c13d19489f81e073870f3f059d07be
André Lessa <andrelessa90@gmail.com>
2025-04-24 01:07:06 -0300

    Estrutura das funções

---
+++ R/functions/aes_proporcional.R
@@ -0,0 +1,9 @@
+library(dplyr)
+
+# ORIENTAÇÃO DO SCRIPT
+# 1. Determinar tamanho da amostra (script separado)
+# 2. Estimadores PPT de Pareto
+# 3. Análise dos Estimadores
+# 4. Comparação com os Parâmetros 
+
+## ORGANIZAR O SCRIPT DE ACORDO COM A ORIENTAÇÃO DELE
---
+++ R/functions/tamanho_amostra.R
@@ -0,0 +1,5 @@
+# Planos
+# AAS
+# AES com alocação proporcional
+# AES com alocação ótima
+# PPT de Pareto
